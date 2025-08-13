c82e4b29900943a5b6c5890e01aba554dccf89b8
Irina Mareeva <mareeva.irina90@gmail.com>
2025-08-13 17:17:29 +0300

    add widget.py

---
+++ src/widget.py
@@ -0,0 +1,22 @@
+from masks import get_mask_card_number, get_mask_account
+
+
+def mask_account_card(card_info: str) -> str:
+    """Маскирует информацию о карте или счете."""
+    last_space_index = card_info.rfind(' ')
+    card_type = card_info[:last_space_index]
+    number = card_info[last_space_index + 1:]
+
+    if card_type.lower() == "Счет":
+        masked_number = get_mask_account(number)
+    else:
+        masked_number = get_mask_card_number(number)
+
+    return f"{card_type} {masked_number}"
+
+
+def get_date(date_string: str) -> str:
+    """Возвращает строку с датой в формате ДД.ММ.ГГГГ"""
+    date_part = date_string.split('T')[0]
+    year, month, day = date_part.split('-')
+    return f"{day}.{month}.{year}"
