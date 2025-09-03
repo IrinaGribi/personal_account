cd3f922be07c347122f75ffda937ee696015ed4a
Irina Mareeva <mareeva.irina90@gmail.com>
2025-09-03 14:41:51 +0300

    update processing

--- src/processing.py
+++ src/processing.py
@@ -1,5 +1,8 @@
 def filter_by_state(data_list: list[dict], state: str = "EXECUTED") -> list[dict]:
     """Фильтрует список словарей по значению ключа 'state'"""
+    if not data_list:
+        raise ValueError("Пустой список")
+
     result = []
     for item in data_list:
         if item.get("state") == state:
@@ -12,6 +15,5 @@ def sort_by_date(data_list: list[dict], reverse: bool = True) -> list[dict]:
     result = []
     for item in data_list:
         result.append(item)
-
     result.sort(key=lambda x: x.get("date", ""), reverse=reverse)
     return result
